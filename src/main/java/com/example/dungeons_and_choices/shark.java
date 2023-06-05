3fccc90c68918a2abd93e77ed34b9c7b0d3883da
PiotrCipior <cipiorp@gmail.com>
2023-06-05 14:36:39 +0200

    4.1

--- src/main/java/com/example/dungeons_and_choices/shark.java
+++ src/main/java/com/example/dungeons_and_choices/shark.java
@@ -12,17 +12,37 @@ public class shark
 {
     @FXML private Button button;
     @FXML private Button button1;
-    @FXML private Text ect;
+    @FXML private Button next;
+    @FXML private Text choice;
     @FXML
     protected void putIn(ActionEvent event) throws IOException {
+        values.sharkAmulet = true;
         button.setVisible(false);
         button.setDisable(true);
         button1.setVisible(false);
         button1.setDisable(true);
-        ect.setVisible(true);
+        choice.setText("""
+                Sięgasz mu do paszczy błagając o to żeby przypadkiem akurat teraz nie postanowił odgryźć Ci ręki… Łapiesz za ząb, pod palcami czujesz że końcówka jest ostra jak szpilka, ale za to na boku czujesz wyżłobienia… Pociągasz lekko i o dziwo, tyle siły wystarczyło. Patrzysz na zdobycz i widzisz napisy oraz sznurek przewiązany za grubszy koniec.""");
+        choice.setVisible(true);
+        next.setDisable(false);
+        next.setVisible(true);
+
     }
     @FXML
     protected void dontPutIn(ActionEvent event) throws IOException {
+        values.deerMask = true;
+        button.setVisible(false);
+        button.setDisable(true);
+        button1.setVisible(false);
+        button1.setDisable(true);
+        choice.setText("""
+                Sięgasz mu do paszczy błagając o to żeby przypadkiem akurat teraz nie postanowił odgryźć Ci ręki… Łapiesz za ząb, pod palcami czujesz że końcówka jest ostra jak szpilka, ale za to na boku czujesz wyżłobienia… Pociągasz lekko i o dziwo, tyle siły wystarczyło. Patrzysz na zdobycz i widzisz napisy oraz sznurek przewiązany za grubszy koniec.""");
+        choice.setVisible(true);
+        next.setDisable(false);
+        next.setVisible(true);
+    }
+    @FXML
+    protected void run(ActionEvent event) throws IOException {
         //SceneController.switchToScene(event,"sharkScene");
     }
 }
---
+++ src/main/java/com/example/dungeons_and_choices/values.java
@@ -0,0 +1,34 @@
+package com.example.dungeons_and_choices;
+
+public class values {
+    public static boolean deerMask = false;
+    public static boolean sharkAmulet = false;
+    public static boolean oldManGift = false;
+    public static boolean naked = false;
+    public static boolean kingsGift = false;
+    public static boolean hooligans = false;
+
+    public boolean isOldManGift() {
+        return oldManGift;
+    }
+
+    public boolean isDeerMask() {
+        return deerMask;
+    }
+
+    public boolean isHooligans() {
+        return hooligans;
+    }
+
+    public boolean isKingsGift() {
+        return kingsGift;
+    }
+
+    public boolean isNaked() {
+        return naked;
+    }
+
+    public boolean isSharkAmulet() {
+        return sharkAmulet;
+    }
+}
